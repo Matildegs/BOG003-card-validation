efc1a1073e3dddbee48c40031a79dde24f9d4fae
Matildegs <matildegonzalez0604@gmail.com>
2021-07-24 23:07:05 -0500

    Pruebas addEventListener

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import validator from './validator.js';
 console.log(validator);
 
 // Pruebas.
-
+document.getElementById("Continuar").addEventListener("click", function (){document.getElementById ("Pagina2")});
 // Función Mostrar Pagina 2.
- const targetDiv = document.getElementById("Pagina2");
- const btn = document.getElementById("Continuar");
- btn.onclick = function () {
-   if (targetDiv.style.display !== "none") {
-     targetDiv.style.display = "none";
-    } 
-   else {
-     targetDiv.style.display = "block";
-    }
-  };
+const targetDiv = document.getElementById("Pagina2");
+const btn = document.getElementById("Continuar");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+   } 
+  else {
+    targetDiv.style.display = "block";
+   }
+ };
