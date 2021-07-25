260f263f60ac23eeb137ffff546560f40a16e7d1
Matildegs <matildegonzalez0604@gmail.com>
2021-07-24 22:42:42 -0500

    Función Mostrar Pagina 2.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import validator from './validator.js';
 console.log(validator);
 
 // Pruebas.
-const targetDiv = document.getElementById("Pagina2");
-const btn = document.getElementById("Continuar");
-btn.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
-  } else {
-    targetDiv.style.display = "block";
-  }
-};
+
+// Función Mostrar Pagina 2.
+ const targetDiv = document.getElementById("Pagina2");
+ const btn = document.getElementById("Continuar");
+ btn.onclick = function () {
+   if (targetDiv.style.display !== "none") {
+     targetDiv.style.display = "none";
+    } 
+   else {
+     targetDiv.style.display = "block";
+    }
+  };
