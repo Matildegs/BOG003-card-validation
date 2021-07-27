5a76c92f81396bf996880fff6519b63ec16097cc
Matildegs <matildegonzalez0604@gmail.com>
2021-07-27 08:31:08 -0500

    MOSTRAR Y OCULTAR DISPLAY

--- src/index.js
+++ src/index.js
@@ -3,10 +3,31 @@ import validator from './validator.js';
 console.log(validator);
 
 // Pruebas.
-document.getElementById("Continuar").addEventListener("click", function (){document.getElementById ("Pagina2")});
+document.getElementById("Continuar").addEventListener("click", function () {
+  const targetDiv = document.getElementById("Pagina2");
+  targetDiv.style.display = "block";
+  console.log(targetDiv.style.display)
+  /*if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+   } 
+  else {
+    targetDiv.style.display = "block";
+   }*/
+   console.log(targetDiv.style.display)
+   const targetDivPagina1 = document.getElementById("Pagina1");
+   targetDivPagina1.style.display = "none";
+   console.log(targetDivPagina1.style.display)
+   /*if (targetDivPagina1.style.display !== "none") {
+     targetDivPagina1.style.display = "none";
+    } 
+   else {
+    targetDivPagina1.style.display = "block";
+    }*/
+  })
+
 // Función Mostrar Pagina 2.
-const targetDiv = document.getElementById("Pagina2");
-const btn = document.getElementById("Continuar");
+
+/*const btn = document.getElementById("Continuar");
 btn.onclick = function () {
   if (targetDiv.style.display !== "none") {
     targetDiv.style.display = "none";
@@ -14,4 +35,4 @@ btn.onclick = function () {
   else {
     targetDiv.style.display = "block";
    }
- };
+ };*/
